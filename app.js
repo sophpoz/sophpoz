e60d4c076be1b18716791319ad3f8069ddd6c783
Sophia Pozzi <sopozzi@gmail.com>
2017-03-29 19:30:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$(".opening,.uxDiv,.wdH1,.magic,.projImg, .myPic ").delay(300).animate({"opacity": "1"}, 700);
+	// $('#ham').click(function({
+	// 	$('.hi').toggleClass('main');
+	// }))
+
+
+})
